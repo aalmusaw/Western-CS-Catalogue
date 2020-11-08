b2a930fc5135e5d5ad006cefa6253d6b774c4a49
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-08 13:35:58 -0500

    made search get req

--- private/models/search.php
+++ private/models/search.php
@@ -1,7 +1,20 @@
 <?php
 include 'connectdb.php';
-$params = $_GET['q'];
-echo $params;
+$entity = $_GET['entity'];
+if ($entity==='university') {
+    $query = 'SELECT name FROM university ORDER BY province_abv;';
+    $result = mysqli_query($connection,$query);
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<option value="';
+        echo $row["name"];
+        echo '"> ';
+        echo $row["name"];
+        echo '</option>';
+        echo '\r\n';
+   }
+   mysqli_free_result($result);
+
+}
 
 
 
