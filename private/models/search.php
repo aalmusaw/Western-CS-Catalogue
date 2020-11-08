13c0bb31915370b0773f5cb8559ee24634f6c7d7
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-08 15:29:02 -0500

    debugging search wcs

--- private/models/search.php
+++ private/models/search.php
@@ -3,20 +3,7 @@
 include 'connectdb.php';
 $entity = $_GET['entity'];
 if ($entity==='wcs_course') {
-    // need these lines in case someone submits invalid query parameters purposely
-    if ($_GET['order_by'] === 'course_code') {
-        $order_by = 'course_code';
-    }
-    else if ($_GET['order_by'] === 'course_name') {
-        $order_by = 'course_name';
-    }
-    if ($_GET['order_dir'] === 'asc') {
-        $order_dir = 'ASC';
-    }
-    else if ($_GET['order_dir'] === 'desc') {
-        $order_dir = 'DESC';
-    }
-    $query = 'SELECT * FROM wcs_course ORDER BY ' . $order_by . ' ' . $order_dir;
+    $query = 'SELECT * FROM wcs_course ORDER BY ' . $order_by . ' ' . $order_dir . ';';
     $result = mysqli_query($connection,$query);
     $cols = array('course_code', 'course_name', 'weight', 'suffix');
     while ($row = mysqli_fetch_assoc($result)) {
