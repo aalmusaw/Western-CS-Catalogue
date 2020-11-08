22532e2439199b775e8afd64d6de089f64eb2a4a
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-08 14:00:44 -0500

    made search get req

--- js/view_functions.js
+++ js/view_functions.js
@@ -28,14 +28,31 @@ function onSelectUniversity() {
     to_hide[1].style.display = 'none';
 
     // load uni choices
+    resetUniNames();
+}
+
+function onSelectEq() {
+    // hide divs for other searches
+    let to_show = document.getElementById('eq');
+    let to_hide = [document.getElementById('wcs_course'), document.getElementById('university')];
+    to_show.style.display = 'block';
+    to_hide[0].style.display = 'none';
+    to_hide[1].style.display = 'none';
+}
+
+function resetUniNames() {
+    // clear options to prevent duplications
+    $( '#uni_name' ).empty()
+
+    // do ajax request to get uni names
     $.ajax({
-        url: "../models/search.php",
-        type: "get",
+        url: '../models/search.php',
+        type: 'get',
         data: { 
           entity: 'university'
         },
         success: function(response) {
-            let $uni_name_select = $( "#uni_name" ), 
+            let $uni_name_select = $( '#uni_name' ), 
             str = response, 
             html = jQuery.parseHTML( str ), 
             nodeNames = []; 
@@ -46,13 +63,4 @@ function onSelectUniversity() {
           //Do Something to handle error
         }
       });
-}
-
-function onSelectEq() {
-    // hide divs for other searches
-    let to_show = document.getElementById('eq');
-    let to_hide = [document.getElementById('wcs_course'), document.getElementById('university')];
-    to_show.style.display = 'block';
-    to_hide[0].style.display = 'none';
-    to_hide[1].style.display = 'none';
 }
