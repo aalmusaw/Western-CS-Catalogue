a8ec04e07d930463bb2f9bc59725c606c49be4f4
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-11 18:48:31 -0500

    document and refactor view_functions.js

--- js/view_functions.js
+++ js/view_functions.js
@@ -53,9 +53,9 @@ let entity = $('#entity').val();
  * and shows relevant (Western Courses) div.
  */
 function onSelectWCSCourse() {
-    $('#wcs_course').show();
-    $('#university').hide();
-    $('#eq').hide();
+  $('#wcs_course').show();
+  $('#university').hide();
+  $('#eq').hide();
 }
 
 /**
@@ -65,15 +65,15 @@ function onSelectWCSCourse() {
  * search criteria. 
  */
 function onSelectUniversity() {
-    // hide dive irrelevant divs
-    $('#university').show();
-    $('#wcs_course').hide();
-    $('#eq').hide();
+  // hide dive irrelevant divs
+  $('#university').show();
+  $('#wcs_course').hide();
+  $('#eq').hide();
 
-    // load more search options
-    resetUniNames();
-    resetUniProv();
-    listUnisByEq();
+  // load more search options
+  resetUniNames();
+  resetUniProv();
+  listUnisByEq();
 }
 
 /**
@@ -83,10 +83,10 @@ function onSelectUniversity() {
  * search for their equivalent counterparts.
  */
 function onSelectEq() {
-    $('#eq').show();
-    $('#wcs_course').hide()
-    $('#university').hide();
-    resetWCSlist();
+  $('#eq').show();
+  $('#wcs_course').hide()
+  $('#university').hide();
+  resetWCSlist();
 }
 
 /**
@@ -96,274 +96,207 @@ function onSelectEq() {
  * to the table with `id=wcs_table`.
  */
 function resetWesternCSTable() {
-    $( '#wcs_table' ).empty()
-    $.get('../controllers/search.php',
-    {
-      entity: 'wcs_course',
-      order_by: $('#wcs_ordered_by').val(),
-      order_dir: $('#wcs_order_dir').val()
-    },
-    function(response) {
-      $('#wcs_table').append(response);
-    }
-    )
-
-    // $.ajax({
-    //     url: '../controllers/search.php',
-    //     type: 'get',
-    //     data: { 
-    //       entity: 'wcs_course',
-    //       order_by: $('#wcs_ordered_by').val(),
-    //       order_dir: $('#wcs_order_dir').val()
-    //     },
-    //     success: function(response) {
-    //         let $wcs_table_body = $( '#wcs_table' ), 
-    //         str = response, 
-    //         html = jQuery.parseHTML( str ), 
-    //         nodeNames = []; 
-             
-    //       $wcs_table_body.append( html );
-    //     },
-    //     error: function(xhr) {
-    //       //Do Something to handle error
-    //     }
-    //   });
-
+  $( '#wcs_table' ).empty();
+  // make GET request
+  $.get('../controllers/search.php',
+  {
+    entity: 'wcs_course',
+    order_by: $('#wcs_ordered_by').val(),
+    order_dir: $('#wcs_order_dir').val()
+  },
+  function(response) {
+    $('#wcs_table').append(response);
+  });
 }
 
+/**
+ * This function makes a GET request to the backend
+ * server and receives select options that contain data
+ * on Universities. The function appends the html
+ * to the select element with `id=uni_name`.
+ */
 function resetUniNames() {
-    // clear options to prevent duplications
-    $( '#uni_name' ).empty()
-    $( '#uni_name' ).append('<option value="-">-</option>')
-
-    // do ajax request to get uni names
-    $.ajax({
-        url: '../controllers/search.php',
-        type: 'get',
-        data: { 
-          entity: 'university',
-          droplist: 'university'
-        },
-        success: function(response) {
-            let $uni_name_select = $( '#uni_name' ), 
-            str = response, 
-            html = jQuery.parseHTML( str ), 
-            nodeNames = []; 
-             
-          $uni_name_select.append( html );
-        },
-        error: function(xhr) {
-          //Do Something to handle error
-        }
-      });
+  // deletes old options to prevent duplications
+  $('#uni_name').empty();
+  $('#uni_name').append('<option value="-" selected>-</option>');
+  // make GET request
+  $.get('../controllers/search.php',
+  {
+    entity: 'university',
+    droplist: 'university'
+  },
+  function(response) {
+    $('#uni_name').append(response);
+  });
 }
 
+/**
+ * This function makes a GET request to the backend
+ * server and receives select options that contain data
+ * on University provinces. The function appends the html
+ * to the select element with `id=uni_province_abv`.
+ */
 function resetUniProv() {
-  // clear options to prevent duplications
-  $( '#uni_province_abv' ).empty()
-  $( '#uni_province_abv' ).append('<option value="-">-</option>')
+  // deletes old options to prevent duplications
+  $('#uni_province_abv').empty();
+  $('#uni_province_abv').append('<option value="-" selected>-</option>');
+  // make GET request
+  $.get('../controllers/search.php',
+  { 
+    entity: 'university',
+    droplist: 'province_abv'
+  },
+  function(response) {
+    $('#uni_province_abv').append(response);
+  });
 
-      // do ajax request to get uni names
-      $.ajax({
-        url: '../controllers/search.php',
-        type: 'get',
-        data: { 
-          entity: 'university',
-          droplist: 'province_abv'
-        },
-        success: function(response) {
-            let $uni_prov_select = $( '#uni_province_abv' ), 
-            str = response, 
-            html = jQuery.parseHTML( str ), 
-            nodeNames = []; 
-             
-          $uni_prov_select.append( html );
-        },
-        error: function(xhr) {
-          //Do Something to handle error
-        }
-      });
-}
-
+/**
+ * This function makes a GET request to the backend
+ * server and receives table rows that contain data
+ * on each selected university. It also hides
+ * irrelvant tables from view. Finally, it loads
+ * the courses offered by the selected University.
+ */
 function fetchUniInfo() {
-  // clear old info if there is any
-  $( '#uni_profile' ).empty();
-  $( '#uni_courses' ).empty();
+  // delete old data to prevent duplications
+  $('#uni_profile').empty();
+  $('#uni_courses').empty();
   // clear unrelated info and selections
-  document.getElementById('uni_province_abv').value = '-';
+  $('#uni_province_abv').val('-');
   $('#uni_prov_table').hide();
   $('#unis_by_eq_table').hide();
   $('#uni_detailed_tables').show();
   $('#uni_offers_eq').prop('checked', false);
-
-  
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: { 
-      entity: 'university',
-      uni_name: $( '#uni_name' ).val()
-    },
-    success: function(response) {
-        let $uni_profile_select = $( '#uni_profile' ), 
-        str = response, 
-        html = jQuery.parseHTML( str ), 
-        nodeNames = []; 
-         
-        $uni_profile_select.append( html );
-    },
-    error: function(xhr) {
-      //Do Something to handle error
-    }
+  // make GET request
+  $.get('../controllers/search.php',
+  {
+    entity: 'university',
+    uni_name: $( '#uni_name' ).val()
+  },
+  function(response) {
+    $('#uni_profile').append(response);
   });
+  // load the selected University's course
   fetchCourseInfo();
 }
 
+/**
+ * This function makes a GET request to the
+ * the backend server and receives table rows 
+ * containing course info offered by the selected
+ * university.
+ */
 function fetchCourseInfo() {
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: { 
-      entity: 'ocs_course',
-      uni_name: $( '#uni_name' ).val()
-    },
-    success: function(response) {
-        let $uni_courses_select = $( '#uni_courses' ), 
-        str = response, 
-        html = jQuery.parseHTML( str ), 
-        nodeNames = []; 
-         
-        $uni_courses_select.append( html );
-    },
-    error: function(xhr) {
-      //Do Something to handle error
-    }
+  // make a GET request
+  $.get('../controllers/search.php',
+  { 
+    entity: 'ocs_course',
+    uni_name: $( '#uni_name' ).val()
+  },
+  function(response) {
+    $('#uni_courses').append(response);
   });
 }
 
-function clearUniInfo() {
-  $( '#uni_detailed_tables' ).hide();
-}
-
+/**
+ * This function makes a GET request to the backend server
+ * and retrieves table rows that contain university info
+ * based on the selected province.
+ */
 function fetchUniList() {
   // clear unrelated info
-  document.getElementById('uni_name').value = '-';
-  clearUniInfo();
+  $('#uni_name').val('-');
+  $('#uni_detailed_tables').hide();
   $('#unis_by_eq_table').hide();
   $('#uni_offers_eq').prop('checked', false);
-
-
   // show uni list and clear old data
   $('#uni_prov_table').show();
   $( '#uni_prov_tbody' ).empty();
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: { 
-      entity: 'university',
-      prov: $( '#uni_province_abv' ).val()
-    },
-    success: function(response) {
-        let $uni_prov_tbody_select = $( '#uni_prov_tbody' ), 
-        str = response, 
-        html = jQuery.parseHTML( str ), 
-        nodeNames = []; 
-         
-        $uni_prov_tbody_select.append( html );
-    },
-    error: function(xhr) {
-      //Do Something to handle error
-    }
+  $.get('../controllers/search.php',
+  { 
+    entity: 'university',
+    prov: $('#uni_province_abv').val()
+  },
+  function(response) {
+    $('#uni_prov_tbody').append(response);
   });
 }
 
+/**
+ * This function makes a GET request to the backend server
+ * and retrieves a list of Universities depending on the
+ * whether the user wants Universities offering Equivalent
+ * courses. 
+ */
 function listUnisByEq() {
   // hide other uni divs and clear selection
   $('#uni_name').val('-');
   $('#uni_province_abv').val('-');
   $('#uni_prov_table').hide();
-  clearUniInfo();
+  $('#uni_detailed_tables').hide();
   // clear old data
   $('#unis_by_eq').empty();
-  // unhide the table
+  // unhide the relevant table
   $('#unis_by_eq_table').show();
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: { 
-      entity: 'university',
-      by_eq: $('#uni_offers_eq').is(':checked')
-    },
-    success: function(response) {
-        let $unis_by_eq_select = $( '#unis_by_eq' ), 
-        str = response, 
-        html = jQuery.parseHTML( str ), 
-        nodeNames = []; 
-         
-        $unis_by_eq_select.append( html );
-    },
-    error: function(xhr) {
-      //Do Something to handle error
-    }
+  // make a GET request
+  $.get('../controllers/search.php',
+  { 
+    entity: 'university',
+    by_eq: $('#uni_offers_eq').is(':checked')
+  },
+  function(response) {
+    $('#unis_by_eq').append(response);
   });
 }
 
+/**
+ * This function makes a GET request to the backend server
+ * and retrieves a list of options for the select element
+ * with `id=eq_wcs_dropbox` and appends them to it. The options
+ * are Western courses that have equivalent courses. 
+ */
 function resetWCSlist() {
+  // clear old data
   $('#eq_wcs_dropbox').empty();
   $('#eq_wcs_dropbox').append('<option value="">-</option>');
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: { 
-      entity: 'eq',
-      dropbox: 'wcs'
-    },
-    success: function(response) {
-        let $eq_wcs_dropbox_select = $( '#eq_wcs_dropbox' ), 
-        str = response, 
-        html = jQuery.parseHTML( str ), 
-        nodeNames = []; 
-         
-      $eq_wcs_dropbox_select.append( html );
-    },
-    error: function(xhr) {
-      //Do Something to handle error
-    }
+  $.get('../controllers/search.php',
+  { 
+    entity: 'eq',
+    dropbox: 'wcs'
+  },
+  function(response) {
+    $('#eq_wcs_dropbox').append(response);
   });
-
 }
 
+
+/**
+ * This function makes a GET request to the backend server
+ * and retrieves a list of table rows containing equivalent courses to
+ * a Western course selected by the user. It also hides irrelevant tables.
+ */
 function eq_ocs_by_wcs() {
   // hide courses by date
   $('#eq_ocs_by_date_table').hide();
   // clear current table and show it
   $('#eq_ocs_by_wcs_table').show();
   $('#eq_ocs_by_wcs').empty();
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: {
-      entity: 'eq',
-      by: 'wcs',
-      wcs: $('#eq_wcs_dropbox').val()
-    },
-    success: function(response) {
-      let eq_ocs_by_wcs_select = $('#eq_ocs_by_wcs'),
-      str = response,
-      html = jQuery.parseHTML(str),
-      nodeNames = [];
-
-      eq_ocs_by_wcs_select.append(html);
-    },
-    error: function(xhr) {
-      // to do
-    }
-  }
-
-  );
-
+  // make GET request
+  $.get('../controllers/search.php',
+  {
+    entity: 'eq',
+    by: 'wcs',
+    wcs: $('#eq_wcs_dropbox').val()
+  },
+  function(response) {
+    $('#eq_ocs_by_wcs').append(response);
+  });
 }
-
+/**
+ * This function makes a GET request to the backend server
+ * and retrieves a list of courses made equivalent on or 
+ * before a date that the user picks. 
+ */
 function eq_ocs_by_date() {
   // hide courses by wcs and clear selection
   $('#eq_ocs_by_wcs_table').hide();
@@ -371,30 +304,14 @@ function eq_ocs_by_date() {
   // clear current table and show it
   $('#eq_ocs_by_date_table').show();
   $('#eq_ocs_by_date').empty();
-
-
-
-  $.ajax({
-    url: '../controllers/search.php',
-    type: 'get',
-    data: {
-      entity: 'eq',
-      by: 'date',
-      date: $('#approval_date').val()
-    },
-    success: function(response) {
-      let eq_ocs_by_date_select = $('#eq_ocs_by_date'),
-      str = response,
-      html = jQuery.parseHTML(str),
-      nodeNames = [];
-
-      eq_ocs_by_date_select.append(html);
-    },
-    error: function(xhr) {
-      // to do
-    }
-  }
-
-  );
-
+  // make a GET request
+  $.get('../controllers/search.php',
+  {
+    entity: 'eq',
+    by: 'date',
+    date: $('#approval_date').val()
+  },
+  function(response) {
+    $('#eq_ocs_by_date').append(response);
+  });
 }
