f4922561ab04265d86a6c0cf21a06c5c340a1447
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-10 23:55:11 -0500

    insert

--- js/insert_functions.js
+++ js/insert_functions.js
@@ -43,8 +43,8 @@ function getUniversityList() {
             $('#EFuniversity').append(response);
             }
     );
-    console.log("First Child of Uni: " + $('#EFuniversity>:first-child').val());
-    getOutsideCourseList($('#EFuniversity>:first-child').val());
+    console.log("First Child of Uni: " + $('#EFuniversity select:first-child').val());
+    getOutsideCourseList($('#EFuniversity select:first-child').val());
 }
 
 function getOutsideCourseList(university_id) {
