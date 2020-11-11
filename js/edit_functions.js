a25d82a80ff63c256b3afe18e17e1fbfb0439fce
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-10 19:15:15 -0500

    receive courses in edit

--- js/edit_functions.js
+++ js/edit_functions.js
@@ -3,7 +3,9 @@ function loadUpCourses() {
     $.getJSON("../controllers/edit.php",
      {entity:"wcs_course"},
      function(response) {
+        console.log(response);
          response = JSON.parse(response);
+         console.log(response);
          let n = Object.keys(response).length;
          for(let i = 0; i < n; i++) {
              courses.push[response[i]];
