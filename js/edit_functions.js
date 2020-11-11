774c27a8794954399fa22c6523410a3d0e68c784
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-10 19:18:39 -0500

    receive courses in edit

--- js/edit_functions.js
+++ js/edit_functions.js
@@ -3,11 +3,10 @@ function loadUpCourses() {
     $.getJSON("../controllers/edit.php",
      {entity:"wcs_course"},
      function(response) {
-         let n = Object.keys(response).length;
-         for(let i = 0; i < n; i++) {
+         for(let i = 0; i < length(response); i++) {
              courses.push[response[i]];
-             let html = "<option value=" + courses[i]["course_code"] + ">" + courses[i]["course_code"]+": "
-                + courses[i]["course_name"] + "</option>" 
+             let html = "<option value=" + courses[i].course_code + ">" + courses[i].course_code + ": "
+                + courses[i].course_name + "</option>" 
              $('wcs_course_list').append(html);
          }
      }
