4cab93279758732e830778d01deb19670cd64374
Ali Al-Musawi <aalmusaw@uwo.ca>
2020-11-10 19:21:37 -0500

    receive courses in edit

--- js/edit_functions.js
+++ js/edit_functions.js
@@ -3,10 +3,12 @@ function loadUpCourses() {
     $.getJSON("../controllers/edit.php",
      {entity:"wcs_course"},
      function(response) {
+         console.log(typeof response);
+         console.log(typeof response[0]);
          for(let i = 0; i < response.length; i++) {
              courses.push[response[i]];
              let html = "<option value=" + courses[i].course_code + ">" + courses[i].course_code + ": "
-                + courses[i].course_name + "</option>" 
+                + courses[i].course_name + "</option>";
              $('wcs_course_list').append(html);
          }
      }
